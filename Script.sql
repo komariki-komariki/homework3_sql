0bfbffbafa2c79dccc9771be0163b254d540db5c
komariki-komariki <komaroff.ilya.s@gmail.com>
2022-03-27 19:10:55 +0300

    init commit

---
+++ Script.sql
@@ -0,0 +1,43 @@
+create table if not exists Genre (
+	Genre_id serial primary key,
+	genre_title varchar(50) not null
+);
+
+create table if not exists Artist (
+	Artist_id serial primary key,
+	Artist_name varchar(50) not null unique
+);
+
+create table if not exists Album(
+	Album_id serial primary key,
+	Year integer not null,
+	Album_title varchar(100) not null
+);
+
+create table if not exists Genre_artist(
+	Genre_id integer references Genre(Genre_id) not null,
+	Artist_id integer references Artist(Artist_id) not null
+);
+
+create table if not exists Album_artist(
+	Album_id integer references Album(Album_id) not null,
+	Artist_id integer references Artist(Artist_id) not null
+);
+
+create table if not exists Track(
+	Track_id serial primary key,
+	duration integer not null,
+	song_title varchar(100) not null,
+	Album_Id integer references Album(Album_id) not null
+);
+
+create table if not exists Collection(
+	Collection_id serial primary key,
+	Year integer not null,
+	Title varchar(100) not null
+);
+
+create table if not exists Tracks_collection(
+	Collection_id integer references Collection(Collection_id) not null,
+	Track_id integer references Track(Track_id) not null
+);
